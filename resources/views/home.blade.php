b243b774aeb68198482d0be24bd8e1af4b376c3d
isabella <isabella24ti@mahasiswa.pcr.ac.id>
2025-09-25 08:58:51 +0700

    Second commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Laravel Web</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boostrap@5.3.0-alpha1/dist/css/boostrap.min.css">
+</head>
+<style>
+    body {
+        font-family: 'Arial', sans-serif;
+    }
+
+    .navbar-brand {
+        font-weight: bold;
+    }
+
+    .navbar {}
+</style>
+
+<body>
+    <nav class="na"></nav>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 use Illuminate\Support\Facades\Route;
@@ -33,3 +34,6 @@ Route::get('/matakuliah/{param1?}', [MatakuliahController::class, 'show']);
 Route::get('/matakuliah/index/{param1}', [MatakuliahController::class, 'index']);
 Route::get('/matakuliah/edit/{param1}', [MatakuliahController::class, 'edit']);
 Route::get('/matakuliah/hapus/{param1}', [MatakuliahController::class, 'destroy']);
+
+//P3
+Route::get('/home' . [HomeController::class, 'index']);
