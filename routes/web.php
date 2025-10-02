3bf936c75e184d50502002d6a8368ce0b1cafc01
isabella <isabella24ti@mahasiswa.pcr.ac.id>
2025-10-02 08:31:21 +0700

    Third Commit

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,41 @@
 <?php
+
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\MatakuliahController;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/pcr', function () {
-    return 'Selamat datang di Website Kampus PCR !';
-});
-Route::get('/mahasiswa', function () {
-    return 'Halo Mahasiswa';
-});
-Route::get('/nama/{param1}', function ($param1) {
-    return 'Nama Saya : ' . $param1;
-});
-Route::get('/nim/{param1}', function ($param1 = '') {
-    return 'nim Saya : ' . $param1;
-});
-Route::get('/nim/{param1}', function ($param1 = '') {
-    return 'nim Saya : ' . $param1;
-});
-Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
-
-// Route::get('/matakuliah/show/{param1?}', [MatakuliahController::class, 'show']);
-// Route::get('/matakuliah/show/{param1?}', function ($param1 = '') {
-//     return view('halaman-st445');
+// Route::get('/pcr', function () {
+//     return 'Selamat datang di Website Kampus PCR !';
 // });
+// Route::get('/mahasiswa', function () {
+//     return 'Halo Mahasiswa';
+// });
+// Route::get('/nama/{param1}', function ($param1) {
+//     return 'Nama Saya : ' . $param1;
+// });
+// Route::get('/nim/{param1}', function ($param1 = '') {
+//     return 'nim Saya : ' . $param1;
+// });
+// Route::get('/nim/{param1}', function ($param1 = '') {
+//     return 'nim Saya : ' . $param1;
+// });
+// Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 
-Route::get('/matakuliah/{param1?}', [MatakuliahController::class, 'show']);
+// // Route::get('/matakuliah/show/{param1?}', [MatakuliahController::class, 'show']);
+// // Route::get('/matakuliah/show/{param1?}', function ($param1 = '') {
+// //     return view('halaman-st445');
+// // });
 
-Route::get('/matakuliah/index/{param1}', [MatakuliahController::class, 'index']);
-Route::get('/matakuliah/edit/{param1}', [MatakuliahController::class, 'edit']);
-Route::get('/matakuliah/hapus/{param1}', [MatakuliahController::class, 'destroy']);
+// Route::get('/matakuliah/{param1?}', [MatakuliahController::class, 'show']);
+
+// Route::get('/matakuliah/index/{param1}', [MatakuliahController::class, 'index']);
+// Route::get('/matakuliah/edit/{param1}', [MatakuliahController::class, 'edit']);
+// Route::get('/matakuliah/hapus/{param1}', [MatakuliahController::class, 'destroy']);
 
 //P3
-Route::get('/home' . [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index']);
+Route::post('question/store', [QuestionController::class, 'store'])
+->name('question.store');
